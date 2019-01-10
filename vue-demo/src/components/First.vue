2aee91c3e33ac57e97819e5ebfe366dcb1de20ab
taodawn <taodawn@gmail.com>
2019-01-10 21:57:54 +0800

    init vue demo

---
+++ vue-demo/src/components/First.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="first-app">
+        {{msg}}
+        <confirm text="register" @message2="getMessage2"></confirm>
+    </div>
+</template>
+<script>
+import Confirm from './sub/Confirm'
+export default {
+    name: "First",
+    components: {
+        Confirm
+    },
+    data(){
+        return{
+            msg: "Welcome to First"
+        };
+    },
+    methods:{
+        getMessage2(val){
+            alert(val);
+        }
+    }
+};
+</script>
+<style>
+
+</style>
---
+++ vue-demo/src/components/sub/Confirm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="confirm-button">
+        <button @click="getButtonClick">{{text || 'OK' }}</button>
+    </div>
+</template>
+<script>
+export default {
+    name: "confirm-button",
+    props: ["text"],
+    data(){
+        return{
+            msg: true
+        }
+    },
+    methods: {
+        getButtonClick() {
+            this.$emit("message2", this.msg);
+        }
+    }
+};
+</script>
+
