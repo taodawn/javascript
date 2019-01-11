af45c7917808d70dfb4d718b5676f7997559509a
TaoHarry <harry.tao@mkcorp.com>
2019-01-11 18:03:32 +0800

    update

---
+++ myadminify/src/router.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+function route(path, file, name, children) {
+  return {
+    exact: true,
+    path,
+    name,
+    children,
+    component: require(`./pages/${file}.vue`)
+  }
+}
+const router = new Router({
+  base: __dirname,
+  mode: 'hash',
+  scrollBehavior: () => ({ y: 0 }),
+  routes: [
+    route('/login', 'Login', 'login'),
+    route('/error', 'Error', 'error')
+  ]
+})
+
+
+export default router
