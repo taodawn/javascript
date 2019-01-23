ad1212ef744f74239e11ca32573fbed21dac2245
TaoHarry <harry.tao@mkcorp.com>
2019-01-23 17:44:56 +0800

    update

--- vuetify-demo/src/main.js
+++ vuetify-demo/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 // import App from './App.vue'
-import App from './examples/layouts/googleKeep'
-
+import App from './examples/layouts/sandbox'
+import '@mdi/font/css/materialdesignicons.css'
 Vue.config.productionTip = false
 
 new Vue({
